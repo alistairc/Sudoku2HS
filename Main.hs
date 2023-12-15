4b3875861d1b918bf08a5bfc51ba2c4a9fed8711
Alistair Cooke <alistair.cooke77@gmail.com>
2023-12-15 10:40:43 +0000

    Code imported from replit experiment

---
+++ Main.hs
@@ -0,0 +1,199 @@
+module Main where
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Function ((&))
+import Data.Maybe
+
+main = do
+  let group = Group (Set.fromList [1,2])
+  isComplete group `shouldBe` False
+
+  let group2 = Group (Set.fromList [1..9])
+  isComplete group2 `shouldBe` True
+
+  Set.toList (missingDigits group) `shouldBe` [3,4,5,6,7,8,9]
+
+  selectRow 1 sampleGridPartial `shouldBe` [Nothing, Just 2, Just 3, Nothing, Just 5, Just 6, Nothing, Just 8, Just 9]
+  selectRow 2 sampleGridPartial `shouldBe` [Just 4, Just 5, Just 6, Just 7, Just 8, Just 9, Just 1, Just 2, Just 3]
+  selectRow 10 sampleGridPartial `shouldBe` []
+
+  selectCol 1 sampleGridPartial `shouldBe` [Nothing, Just 4, Just 7, Just 2, Just 5, Just 8, Just 3, Just 6, Just 9]
+  selectCol 5 sampleGridPartial `shouldBe` [Just 5, Just 8, Just 2, Nothing, Just 9, Just 3, Just 7, Just 1, Just 4]
+
+  selectSquare (1,1) sampleGridPartial `shouldBe` [
+    Nothing, Just 2,  Just 3,
+    Just 4,  Just 5,  Just 6,
+    Just 7,  Just 8,  Just 9]
+    
+  selectSquare (3,1) sampleGridPartial `shouldBe` [
+    Nothing, Just 8,  Just 9,
+    Just 1,  Just 2,  Just 3,
+    Just 4,  Just 5,  Just 6]
+
+  selectSquare (1,3) sampleGridPartial `shouldBe` [
+    Just 3,  Just 4,  Nothing,
+    Just 6,  Just 7,  Just 8, 
+    Just 9,  Just 1,  Just 2]
+
+  (sampleGridPartial & selectSquare (1,3) & cellsToGroup & isComplete) `shouldBe` False
+  (sampleGridPartial & selectSquare (1,3) & cellsToGroup & missingDigits) `shouldBe` Set.fromList [5]
+
+  squareContaining (1,1) `shouldBe` (1,1)
+  squareContaining (9,9) `shouldBe` (3,3)
+  squareContaining (2,2) `shouldBe` (1,1)
+  squareContaining (1,4) `shouldBe` (1,2)
+
+  possibleDigits sampleGridSolve (1,1) `shouldBe` Set.fromList []
+  possibleDigits sampleGridSolve (5,2) `shouldBe` Set.fromList [8]
+  possibleDigits sampleGridSolve (5,5) `shouldBe` Set.fromList [1,2,3,4,5,6,7,8,9]
+  possibleDigits sampleGridSolve (4,5) `shouldBe` Set.fromList [2,5,8]
+
+  cellAt (2,1) sampleGridPartial `shouldBe` Just 2
+  cellAt (2,4) sampleGridPartial `shouldBe` Nothing
+  
+  let newGrid = gridWith (1,1) Nothing sampleGridFull
+    in do
+      cellAt (1,1) newGrid `shouldBe` Nothing
+      cellAt (2,1) newGrid `shouldBe` Just 2
+
+  let newGrid = gridWith (3,3) (Just 1) sampleGridPartial
+    in do
+      cellAt (3,3) newGrid `shouldBe` Just 1
+
+  -- nextSolution sampleGridSolve `shouldBe` gridWith (5,1) (Just 5) sampleGridSolve
+  
+
+
+shouldBe :: Show a => a -> a -> IO()
+shouldBe expr expected =
+  if show expr == show expected then
+    putStrLn $ "Passed " ++ show expected
+  else do
+    putStrLn $ "Failed " ++ show expected
+    putStrLn $ "   got " ++ show expr
+
+type Cell = Maybe Int
+newtype Group = Group (Set Int)
+newtype Grid = Grid [Cell]
+
+squareSize = 3
+gridSize = squareSize * squareSize
+
+isComplete :: Group -> Bool
+isComplete group =
+  let (Group digits) = group in
+  Set.size digits == gridSize
+
+missingDigits group =
+  let (Group digits) = group in
+  Set.difference allDigits digits
+
+cellsToGroup :: [Cell] -> Group
+cellsToGroup cells = Group (cells & catMaybes & Set.fromList)
+
+allDigits = Set.fromList [1..gridSize]
+
+sampleGridFull = Grid [
+  Just 1, Just 2, Just 3, Just 4, Just 5, Just 6, Just 7, Just 8, Just 9,
+  Just 4, Just 5, Just 6, Just 7, Just 8, Just 9, Just 1, Just 2, Just 3,
+  Just 7, Just 8, Just 9, Just 1, Just 2, Just 3, Just 4, Just 5, Just 6,
+  Just 2, Just 3, Just 4, Just 5, Just 6, Just 7, Just 8, Just 9, Just 1,
+  Just 5, Just 6, Just 7, Just 8, Just 9, Just 1, Just 2, Just 3, Just 4,
+  Just 8, Just 9, Just 1, Just 2, Just 3, Just 4, Just 5, Just 6, Just 7,
+  Just 3, Just 4, Just 5, Just 6, Just 7, Just 8, Just 9, Just 1, Just 2,
+  Just 6, Just 7, Just 8, Just 9, Just 1, Just 2, Just 3, Just 4, Just 5,
+  Just 9, Just 1, Just 2, Just 3, Just 4, Just 5, Just 6, Just 7, Just 8
+  ]
+
+sampleGridPartial = Grid [
+  Nothing, Just 2,  Just 3,  Nothing, Just 5,  Just 6,  Nothing, Just 8,  Just 9,
+  Just 4,  Just 5,  Just 6,  Just 7,  Just 8,  Just 9,  Just 1,  Just 2,  Just 3,
+  Just 7,  Just 8,  Just 9,  Just 1,  Just 2,  Just 3,  Just 4,  Just 5,  Just 6,
+  Just 2,  Nothing, Just 4,  Just 5,  Nothing, Just 7,  Just 8,  Nothing, Just 1,
+  Just 5,  Just 6,  Just 7,  Just 8,  Just 9,  Just 1,  Just 2,  Just 3,  Just 4,
+  Just 8,  Just 9,  Just 1,  Just 2,  Just 3,  Just 4,  Just 5,  Just 6,  Just 7,
+  Just 3,  Just 4,  Nothing, Just 6,  Just 7,  Nothing, Just 9,  Just 1,  Nothing,
+  Just 6,  Just 7,  Just 8,  Just 9,  Just 1,  Just 2,  Just 3,  Just 4,  Just 5,
+  Just 9,  Just 1,  Just 2,  Just 3,  Just 4,  Just 5,  Just 6,  Just 7,  Just 8
+  ]
+
+sampleGridSolve = Grid [
+  Just 1,  Just 2,  Just 3,  Just 4,  Nothing, Just 6,  Just 7,  Just 8,  Just 9,
+  Just 4,  Just 5,  Just 6,  Just 7,  Nothing, Just 9,  Just 1,  Just 2,  Just 3,
+  Just 7,  Just 8,  Just 9,  Just 1,  Nothing, Just 3,  Just 4,  Just 5,  Just 6,
+  Just 2,  Just 3,  Just 4,  Nothing, Nothing, Nothing, Just 8,  Just 9,  Just 1,
+  Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, 
+  Just 8,  Just 9,  Just 1,  Nothing, Nothing, Nothing, Just 5,  Just 6,  Just 7,
+  Just 3,  Just 4,  Just 5,  Just 6,  Nothing, Just 8,  Just 9,  Just 1,  Just 2,
+  Just 6,  Just 7,  Just 8,  Just 9,  Nothing, Just 2,  Just 3,  Just 4,  Just 5,
+  Just 9,  Just 1,  Just 2,  Just 3,  Nothing, Just 5,  Just 6,  Just 7,  Just 8
+  ]
+
+selectRow :: Int -> Grid -> [Cell]
+selectRow rowNum (Grid cells) = 
+  cells
+  & drop ((rowNum - 1) * gridSize)
+  & take gridSize
+
+selectCol :: Int -> Grid -> [Cell]
+selectCol colNum (Grid cells) =
+  cells
+  & splitEvery gridSize
+  & map (\row -> row & drop (colNum - 1) & head)  -- this line isn't very readable
+
+selectSquare :: (Int, Int) -> Grid -> [Cell]
+selectSquare (x,y) (Grid cells) =
+  let
+    rowsToSkip = (y - 1) * squareSize
+    colsToSkip = (x - 1) * squareSize
+  in
+    cells 
+    & splitEvery gridSize
+    & drop rowsToSkip & take squareSize
+    & map (\row -> row & drop colsToSkip & take squareSize)
+    & concat
+
+squareContaining :: (Int, Int) -> (Int, Int)
+squareContaining (x,y) = 
+  (((x -1) `div` squareSize) + 1, ((y - 1) `div` squareSize) + 1)
+
+possibleDigits :: Grid -> (Int, Int) -> Set Int
+possibleDigits grid (x, y) =
+  let 
+    squarePos = squareContaining (x,y)
+    colGroup = cellsToGroup $ selectCol x grid
+    rowGroup = cellsToGroup $ selectRow y grid
+    squareGroup = cellsToGroup $ selectSquare squarePos grid
+  in
+    foldr Set.intersection allDigits [missingDigits colGroup, missingDigits rowGroup, missingDigits squareGroup]
+
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery n = takeWhile (not.null) . map (take n) . iterate (drop n)
+
+
+gridWith :: (Int, Int) -> Cell -> Grid -> Grid
+gridWith coords cell (Grid cells) =
+  let pos = cellIndex coords
+   in Grid (take pos cells ++ [cell] ++ drop (pos + 1) cells)
+
+cellAt :: (Int, Int) -> Grid -> Cell
+cellAt coords (Grid cells) =
+  cells !! cellIndex coords
+
+cellIndex (x, y) = (y - 1) * gridSize + (x - 1)
+
+allCellCoords =  (,) <$> [1..gridSize] <*> [1..gridSize] 
+
+
+nextSolution :: Grid -> Grid
+nextSolution grid = 
+  Grid $ map (\coord -> newCell $ cellAt coord grid) allCellCoords
+  where 
+    newCell currentCell
+--   go (1,1) grid
+--   where
+--     go (x,y) grid = 
+--       let possible = possibleDigits grid (x,y) & Set.toList
+--        in case possible of
+--          [single] -> gridWith (x,y) single
