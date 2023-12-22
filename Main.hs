257035bb523a93a685511ecadae8deb86374426e
Alistair Cooke <alistair.cooke77@gmail.com>
2023-12-22 19:31:18 +0000

    Try it out on i's easy sudoku - works!

--- Main.hs
+++ Main.hs
@@ -7,15 +7,17 @@ import Data.Maybe
 
 main = do
   putStrLn $ formatGrid $ solveIter $ Grid [
-    Just 1,  Just 2,  Just 3,  Just 4,  Nothing, Just 6,  Just 7,  Just 8,  Just 9,
-    Just 4,  Just 5,  Just 6,  Just 7,  Nothing, Just 9,  Just 1,  Just 2,  Just 3,
-    Just 7,  Just 8,  Just 9,  Just 1,  Nothing, Just 3,  Just 4,  Just 5,  Just 6,
-    Just 2,  Just 3,  Just 4,  Nothing, Nothing, Nothing, Just 8,  Just 9,  Just 1,
-    Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, 
-    Just 8,  Just 9,  Just 1,  Nothing, Nothing, Nothing, Just 5,  Just 6,  Just 7,
-    Just 3,  Just 4,  Just 5,  Just 6,  Nothing, Just 8,  Just 9,  Just 1,  Just 2,
-    Just 6,  Just 7,  Just 8,  Just 9,  Nothing, Just 2,  Just 3,  Just 4,  Just 5,
-    Just 9,  Just 1,  Just 2,  Just 3,  Nothing, Just 5,  Just 6,  Just 7,  Just 8
+    Just 5,  Just 8,  Just 3,  Nothing, Nothing, Just 4,  Nothing, Just 1,  Nothing,
+    Nothing, Nothing, Just 7,  Just 1,  Nothing, Nothing, Nothing, Nothing, Nothing,
+    Just 9,  Just 2,  Nothing, Just 6,  Just 7,  Nothing, Nothing, Nothing, Nothing,
+
+    Just 2,  Just 7,  Nothing, Nothing, Just 5,  Nothing, Nothing, Nothing, Just 8,
+    Nothing, Nothing, Nothing, Just 2,  Nothing, Just 7,  Nothing, Nothing, Nothing,
+    Just 3,  Nothing, Nothing, Nothing, Just 1,  Nothing, Nothing, Just 7,  Just 6,
+
+    Nothing, Nothing, Nothing, Nothing, Just 4,  Just 6,  Nothing, Just 9,  Just 1,
+    Nothing, Nothing, Nothing, Nothing, Nothing, Just 1,  Just 3,  Nothing, Nothing,
+    Nothing, Just 4,  Nothing, Just 8,  Nothing, Nothing, Just 7,  Just 6,  Just 5
     ]
 
 type Cell = Maybe Int
